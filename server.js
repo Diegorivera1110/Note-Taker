b5781bc084187a168dfaaa23950781eaca8c118c
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-03 23:41:20 -0700

    testing one file functionality

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+
+
 function generateNote(body, noteArray) {
   const newNote = body;
   if (!Array.isArray(noteArray)) noteArray = [];
