05eaf4f95114900b21d25d7ce009d62ddd375acd
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-03 18:36:12 -0700

    added get paths

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+const notesGroup = require('./db/db.json');
 
 app.use(express.static('public'));
 
@@ -11,6 +13,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 // parse incoming json data
 app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+    res.json(notesGroup.slice(1))
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
