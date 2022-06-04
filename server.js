f6c29bf911812bf73416c81be326bc25f9fff07d
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-04 14:25:09 -0700

    heroku push

--- server.js
+++ server.js
@@ -14,47 +14,47 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming json data
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => {
-  res.json(notesLis.slice(1));
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notesLis.slice(1));
+// });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+// app.get("/notes", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/notes.html"));
+// });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 
 
-function generateNote(body, noteArray) {
-  const newNote = body;
-  if (!Array.isArray(noteArray)) 
-  noteArray = [];
+// function generateNote(body, noteArray) {
+//   const newNote = body;
+//   if (!Array.isArray(noteArray)) 
+//   noteArray = [];
 
-  if (noteArray.length === 0)
-   noteArray.push(0);
+//   if (noteArray.length === 0)
+//    noteArray.push(0);
 
-  body.id = noteArray[0];
-  noteArray[0]++;
+//   body.id = noteArray[0];
+//   noteArray[0]++;
 
-  noteArray.push(newNote);
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify(noteArray, null, 2)
-  );
-  return newNote;
-}
+//   noteArray.push(newNote);
+//   fs.writeFileSync(
+//     path.join(__dirname, "./db/db.json"),
+//     JSON.stringify(noteArray, null, 2)
+//   );
+//   return newNote;
+// }
 
-app.post("/api/notes", (req, res) => {
-  const newNote = generateNote(req.body, notesList);
-  res.json(newNote);
-});
+// app.post("/api/notes", (req, res) => {
+//   const newNote = generateNote(req.body, notesList);
+//   res.json(newNote);
+// });
 
 // const deleteNote = (id, notesList) => {
 //   for (let i = 0; i < notesList.length; i++) {
