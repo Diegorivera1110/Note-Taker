51365f5c5c6e9d43eae7d2b6b86550810336b96a
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-03 23:31:07 -0700

    typos corrected

--- server.js
+++ server.js
@@ -1,64 +1,60 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const notesGroup = require('./db/db.json');
+const notesGroup = require("./db/db.json");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// // parse incoming string or array data
-// app.use(express.urlencoded({ extended: true }));
-// // parse incoming json data
-// app.use(express.json());
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming json data
+app.use(express.json());
 
-// app.get('/api/notes', (req, res) => {
-//     res.json(notesGroup.slice(1))
-// });
+app.get("/api/notes", (req, res) => {
+  res.json(notesGroup.slice(1));
+});
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/notes.html'));
-// });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-// function generateNote(body, noteArray) {
-//     const newNote = body;
-//     if (!Array.isArray(noteArray))
-//     noteArray = [];
+function generateNote(body, noteArray) {
+  const newNote = body;
+  if (!Array.isArray(noteArray)) noteArray = [];
 
-//     if (noteArray.length === 0)noteArray.push(0);
+  if (noteArray.length === 0) noteArray.push(0);
 
-//     body.is = noteArray[0];
-//     noteArray[0]++;
+  body.id = noteArray[0];
+  noteArray[0]++;
 
-//     noteArray.push(newNote);
-//     fs.writeFileSync(path.join(__dirname, './db/db.json'),
-//     JSON.stringify(noteArray, null, 2)
-//     );
-//     return newNote;
-// }
+  noteArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(noteArray, null, 2)
+  );
+  return newNote;
+}
 
-//     app.post('/api/notes', (req, res) => {
-//         const newNote = generateNote(req.body, notesGroup);
-//         res.json(newNote);
-//     });
+app.post("/api/notes", (req, res) => {
+  const newNote = generateNote(req.body, notesGroup);
+  res.json(newNote);
+});
 
-
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
+  console.log(`API server now on port ${PORT}`);
 });
